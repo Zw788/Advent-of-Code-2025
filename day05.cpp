d7d40cfe7eb56f4d0ae275335d8c67751e128a7e
Zw788 <31865624+Zw788@users.noreply.github.com>
2025-12-05 18:36:14 +0000

    day05

---
+++ day05.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <chrono>
+
+
+
+using namespace std;
+
+
+int main()
+{
+	auto start = chrono::steady_clock::now();
+	/////////////////////////////////////////////////////////
+	ifstream myfile;
+	myfile.open("../aoc2025_input.txt");
+	if (!myfile.is_open()) {
+		cerr << "Error opening file" << endl;
+		return 1;
+	}
+
+
+
+  
+  myfile.close();
+	/////////////////////////////////////////////////////////
+	auto end = chrono::steady_clock::now();
+	auto diff = end - start;
+  cout << "\n";
+	cout << "Execution time: " << chrono::duration<double, milli>(diff).count() << " ms" << "\n";
+	/////Execution time: 281.713 ms
+	return 0;
+}
